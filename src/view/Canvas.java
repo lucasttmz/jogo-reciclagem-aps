0d19b76bac6635f2916e755c1a8768a6d05498e2
Whoj01 <josueneto9@hotmail.com>
2023-05-05 20:00:16 -0300

    feat: adicionando jogoView funcional

--- src/view/Canvas.java
+++ src/view/Canvas.java
@@ -1,36 +1,34 @@
 package view;
 
 import java.awt.Graphics;
+import java.awt.Image;
 import java.util.List;
+
+import javax.swing.ImageIcon;
 import javax.swing.JPanel;
 import model.Desenhavel;
 
-class Canvas extends JPanel
-{
+class Canvas extends JPanel {
     private List<Desenhavel> desenhaveis;
 
     // Imagem de fundo do canvas
-    // private final Image background = new ImageIcon(
-    //      this.getClass().getResource("/imagens/background.jpg")).getImage();
-    
-    public void setDesenhaveis(List<Desenhavel> desenhaveis)
-    {
+    private final Image background = new ImageIcon(
+            this.getClass().getResource("/resources/background.png")).getImage();
+
+    public void setDesenhaveis(List<Desenhavel> desenhaveis) {
         this.desenhaveis = desenhaveis;
     }
 
     @Override
-    protected void paintComponent(Graphics g)
-    {
+    protected void paintComponent(Graphics g) {
         super.paintComponent(g);
 
         // Desenha a imagem de fundo
-        // g.drawImage(background, 0, 0, null);
+        g.drawImage(background, 0, 0, null);
 
         // Desenha cada um dos desenhaveis
-        if (desenhaveis != null)
-        {
-            for (Desenhavel d : List.copyOf(desenhaveis))
-            {
+        if (desenhaveis != null) {
+            for (Desenhavel d : List.copyOf(desenhaveis)) {
                 g.drawImage(d.getImagem().getImage(), d.getX(), d.getY(), null);
             }
         }
